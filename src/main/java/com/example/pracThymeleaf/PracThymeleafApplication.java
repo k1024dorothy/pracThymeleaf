52f85f824005ec78bd5858b5df12b96a411c5b6d
k1024dorothy <k1024dorothy@gmail.com>
2021-11-12 16:24:55 +0900

    init

---
+++ src/main/java/com/example/pracThymeleaf/PracThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.pracThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracThymeleafApplication.class, args);
+	}
+
+}
